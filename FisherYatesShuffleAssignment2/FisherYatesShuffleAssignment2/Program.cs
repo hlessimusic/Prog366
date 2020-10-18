61f7d599df332c5cb77c31ef98d64e69f9fd90d8
Benjamin Brody <bwlb@comcast.net>
2020-10-18 13:52:13 -0500

    Add files via upload

---
+++ FisherYatesShuffleAssignment2/FisherYatesShuffleAssignment2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FisherYatesShuffleAssignment2
+{
+    class Program
+    {
+        // Program entry point.
+        static void Main()
+        {           
+            // Assigns path for file to be read.
+            const string path = @"..\\Resources\\TestValues.txt";
+            // Load file to array.
+            FileLoader.readFileToArray(path);
+            // Shuffle array.
+            FisherYatesShuffler.DoFisherYatesShuffle(FileLoader.GetArray());
+            // Print array.
+            FileLoader.printArray();
+            // Display Console until user presses key.
+            Console.ReadLine();
+        }
+    }
+}
