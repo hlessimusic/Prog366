f6491e4f06341250ecbd2b190981c08cf76e1bd1
Benjamin Brody <bwlb@comcast.net>
2020-10-18 13:51:41 -0500

    Add files via upload

---
+++ FisherYatesShuffleAssignment2/FisherYatesShuffleAssignment2/Swapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FisherYatesShuffleAssignment2
+{
+    public static class Swapper
+    {
+        public static void SwapValuesAtIndices(this object[] elements, int i, int j)
+        {
+            // Store value at index i in temp
+            object temp = elements[i];
+            // Set value at inex i to value at index j
+            elements[i] = elements[j];
+            // Set value at index j to temp
+            elements[j] = temp;
+        }
+    }
+}
