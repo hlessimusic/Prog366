d451111918ab9ea9742cbc865eac38587aa630ae
Benjamin Brody <bwlb@comcast.net>
2020-10-18 13:47:23 -0500

    Add files via upload

---
+++ FisherYatesShuffleAssignment2/FisherYatesShuffleAssignment2/FileLoader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FisherYatesShuffleAssignment2
+{
+    public static class FileLoader
+    {
+        // Variable declaration for array of strings.
+        private static string[] elements = new string[10];
+
+        // readFileToArray method declaration. Loads elements with data from file located at inputFilePath.
+        public static void readFileToArray(string inputFilePath)
+        {
+            // Starting at the first index, assign input stream from file at inputFilePath to value at current index. Increments current index with line break in file.
+            elements = File.ReadAllLines(inputFilePath);
+        }
+
+        // GetArray method declaration. Returns elements array.
+        public static string[] GetArray()
+        {
+            return elements;
+        }
+
+
+        public static void printArray()
+        {
+            // Iterate over elements.
+            for (int i = 0; i<= 9; i++)
+            {
+                // Prints value in elements at index i to the console.
+                Console.WriteLine(elements[i]);
+            }
+        }
+    }
+}
---
+++ FisherYatesShuffleAssignment2/FisherYatesShuffleAssignment2/FisherYatesShuffler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FisherYatesShuffleAssignment2
+{
+    public static class FisherYatesShuffler
+    {
+        // randNum variable declaration and assignment to new random.
+        private static Random randomNum = new Random();
+
+        // DoFisherYatesShuffle method declaration
+        public static void DoFisherYatesShuffle(this object[] elements)
+        {
+            // Loop over array.
+            for (int i = elements.Length - 1; i > 0; i--)
+            {
+                // set value of j to a random index number between 0 and 9
+                int j = GetRandomNum(i);
+                // Swaps values at indices i and j
+                elements.SwapValuesAtIndices(i, j);
+            }
+        }
+
+        // GetRandomNum method declaration.
+        private static int GetRandomNum(int i)
+        {
+            // return random number less than i + 1
+            return randomNum.Next(i + 1);
+        }
+
+    }
+}
