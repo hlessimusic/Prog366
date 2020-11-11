b8a561040cceef4a45c24659a84e66c671c78ad9
Benjamin Brody <bwlb@comcast.net>
2020-11-10 22:33:11 -0600

    Add files via upload

---
+++ Assignment3_DataStructures/Assignment3_DataStructures/ArrayAndMap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_DataStructures
+{
+    class ArrayAndMap
+    {
+        public static Hashtable myHash = new Hashtable();
+        public static LinkedList<string> myList = new LinkedList<string>();
+
+        //Adding Elements to Table
+        public void addElementToTable(string inputValue, int inputKey)
+        {
+            myHash.Add(inputKey, inputValue);
+        }
+
+        //Adding elements to Linked List
+        public void addElementToList(string inputValue)
+        {
+            myList.AddLast(inputValue);
+        }
+    }
+}
---
+++ Assignment3_DataStructures/Assignment3_DataStructures/FileManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment3_DataStructures
+{
+    class FileManager
+    {
+        public string filePath;
+        public string[] testArray;
+
+        //Fill array with data from file
+        public void loadFileToArray(string inputPath)
+        {
+            filePath = inputPath;
+            testArray = File.ReadAllText(filePath).Split('\t').ToArray();
+        }
+    }
+}
